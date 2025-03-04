7b5e77162ec7f9353bd712f48da6f656daa69a24
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2025-03-04 09:35:15 +0100

    Entferne nicht verwendete Typdefinitionen und Modelle für MergedCell, TableOptions, TableCellStyle und CustomFont aus dem Projekt

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { PdfTable } from './classes/Table';
-export * from './config/DesignConfig';
-export { CustomFont } from './models/CustomFont';
-export { materialDesignConfig } from './config/DesignConfig';
